b06abde180d0d33704ca36474c24d1925f52af66
DannyStephens <danny.stephens1@outlook.com>
2025-04-13 13:23:29 +0100

    Implementing FFUF

--- scanpro.py
+++ scanpro.py
@@ -3,7 +3,6 @@ import os
 import sys
 from datetime import datetime
 
-
 def run_nmap(ip):
     timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
     out_dir = "scan_results"
@@ -17,16 +16,38 @@ def run_nmap(ip):
     with open(outfile, "w") as f:
         subprocess.run(cmd, stdout=f, stderr=subprocess.STDOUT)
 
-    print(f"[+] Scan complete. Output saved to: {outfile}")
+    print(f"[+] Nmap scan complete. Output saved to: {outfile}")
 
+def run_ffuf(ip):
+    timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
+    out_dir = "scan_results"
+    os.makedirs(out_dir, exist_ok=True)
+    outfile = os.path.join(out_dir, f"ffuf_{ip}_{timestamp}.txt")
+
+    print(f"[*] Running FFUF scan on {ip}...")
+
+    wordlist = "/usr/share/dirb/wordlists/common.txt "
+
+    cmd = [
+        "ffuf", "-u", f"http://{ip}/FUZZ", "-w", wordlist, "-t", "50", "-o", outfile
+    ]
+
+    with open(outfile, "w") as f:
+        subprocess.run(cmd, stdout=f, stderr=subprocess.STDOUT)
+
+    print(f"[+] FFUF scan complete. Output saved to: {outfile}")
 
 def main():
     if len(sys.argv) != 2:
-        print("INCORRECT INPUT. Corrected Input: python3 scanpro.py <IP_OR_DOMAIN>")
+        print("Usage: python3 scanpro.py <IP_OR_DOMAIN>")
         sys.exit(1)
 
     target = sys.argv[1].strip()
+    
     run_nmap(target)
+    
+    print("[*] Checking if the target has HTTP(S) services...")
+    run_ffuf(target)
 
 if __name__ == "__main__":
     main()
